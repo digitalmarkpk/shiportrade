4a07b4b69a1819ef1978c3f7ad2881f268a2f005
digitalmarkpk <digitalmarkpkl@gmail.com>
2026-04-24 18:42:22 +0500

    fix: add force-dynamic for useSearchParams

--- src/app/directories/ports/page.tsx
+++ src/app/directories/ports/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { useState, useMemo, useEffect } from "react";
 import dynamic from "next/dynamic";
 import { useRouter, useSearchParams } from "next/navigation";
